8a8bf0dea50a53b88c4f1795824dd62b002663e7
cyrusluczkow <cyrusluczkow@gmail.com>
2017-02-17 10:33:08 -0500

    Add files via upload

---
+++ Block.java
@@ -0,0 +1,106 @@
+package pongLab;
+
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Block implements Locatable
+{
+	private int xPos;
+	private int yPos;
+	private int width;
+	private int height;
+
+	private Color color;
+
+	public Block()
+	{ 
+		
+	}
+
+	//add other Block constructors - x , y , width, height, color
+	
+	public Block(int x, int y, int w, int h)
+	{
+		xPos = x;
+		yPos = y;
+		width = w;
+		height = h;
+	}
+	
+	public Block(int x, int y, int w, int h, Color c)
+	{
+		xPos = x;
+		yPos = y;
+		width = w;
+		height = h;
+		color = c;
+	}
+	
+	public void setPos(int x, int y) {
+		xPos = x;
+		yPos = y;
+		
+	}
+
+	public void setX(int x) {
+		xPos = x;
+		
+	}
+	
+	public void setY(int y) {
+		yPos = y;
+		
+	}
+
+	public int getX() {
+		return xPos;
+	}
+
+	public int getY() {
+		return yPos;
+	}  
+	
+	
+   //add the other set methods
+   
+
+   public void setColor(Color col)
+   {
+
+
+   }
+
+   public void draw(Graphics window)
+   {
+   	//uncomment after you write the set and get methods
+      //window.setColor(color);
+      //window.fillRect(getX(), getY(), getWidth(), getHeight());
+   }
+
+   public void draw(Graphics window, Color col)
+   {
+
+
+   }
+   
+	public boolean equals(Object obj)
+	{
+
+
+
+
+		return false;
+	}
+
+	 
+
+   //add the other get methods
+    
+
+   //add a toString() method  - x , y , width, height, color
+}
